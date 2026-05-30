0c48eff84184667b8f1d2f33a9024fb368a58c73
Kaustubh Ladiya <k4ustu3h@gmail.com>
2026-05-30 23:43:32 +0530

    Split vendor code in different chunks

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,24 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	build: {
+		rollupOptions: {
+			output: {
+				manualChunks: (id) => {
+					if (id.includes("node_modules")) {
+						if (id.includes("@m3e")) return "m3e-vendor";
+						if (id.includes("react")) return "react-vendor";
+						return "vendor";
+					}
+				},
+			},
 		},
 	},
 });
