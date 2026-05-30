c9e0a0c6e77a022cfa431e9af019e024ab7bec58
Kaustubh Ladiya <k4ustu3h@gmail.com>
2026-05-30 13:37:56 +0530

    Add fast-average-color to extract theme color from background image

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from "react";
+
+import { FastAverageColor } from "fast-average-color";
 import { M3eTheme } from "@m3e/react/theme";
 
 import Greeting from "@/components/typography/Greeting";
 
 export default function App() {
+	const [bgUrl] = useState(() => `https://picsum.photos/1920/1080`);
+
+	const [themeColor, setThemeColor] = useState("");
+
+	useEffect(() => {
+		const fac = new FastAverageColor();
+		const img = new Image();
+		img.crossOrigin = "anonymous";
+
+		img.src = bgUrl;
+
+		img.onload = () => {
+			const color = fac.getColor(img);
+			setThemeColor(color.hex);
+			fac.destroy();
+		};
+
+		return () => fac.destroy();
+	}, [bgUrl]);
+
 	return (
-		<M3eTheme>
+		<M3eTheme color={themeColor}>
 			<div
 				style={{
-					backgroundImage: "url(https://picsum.photos/1920/1080)",
+					backgroundImage: `url(${bgUrl})`,
+					backgroundPosition: "center",
+					backgroundSize: "cover",
 					display: "flex",
 					flexDirection: "column",
 					height: "100%",
