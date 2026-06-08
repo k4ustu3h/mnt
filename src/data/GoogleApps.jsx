3214aff04e9da955a2d5dde7b09117ce62e64408
Kaustubh Ladiya <k4ustu3h@gmail.com>
2026-06-08 19:11:50 +0530

    Add more Google apps
    Added
      - @icons-pack/react-simple-icons

--- src/data/GoogleApps.jsx
+++ src/data/GoogleApps.jsx
@@ -1,17 +1,200 @@
 import {
+	SiBlogger,
+	SiChromewebstore,
+	SiGmail,
+	SiGoogle,
+	SiGoogleads,
+	SiGoogleanalytics,
+	SiGooglecalendar,
+	SiGooglechat,
+	SiGoogleclassroom,
+	SiGoogledocs,
+	SiGoogledrive,
+	SiGoogleearth,
+	SiGoogleforms,
+	SiGooglegemini,
+	SiGooglekeep,
+	SiGooglemaps,
+	SiGooglemeet,
+	SiGooglenews,
+	SiGooglephotos,
+	SiGoogleplay,
+	SiGooglesheets,
+	SiGoogleslides,
+	SiGoogletranslate,
+	SiNotebooklm,
+	SiYoutube,
+	SiYoutubemusic,
+} from "@icons-pack/react-simple-icons";
+
+import {
 	AccountCircle,
+	Bookmark,
 	ContactsProduct,
+	Task,
+	VpnKey,
+	Wallet,
 } from "@nine-thirty-five/material-symbols-react/rounded";
 
 export const apps = [
 	{
 		name: "Account",
-		url: "https://myaccount.google.com/",
+		url: "https://myaccount.google.com",
 		icon: <AccountCircle />,
 	},
 	{
+		name: "Search",
+		url: "https://google.com",
+		icon: <SiGoogle />,
+	},
+	{
+		name: "Maps",
+		url: "https://maps.google.com",
+		icon: <SiGooglemaps />,
+	},
+	{
+		name: "YouTube",
+		url: "https://youtube.com",
+		icon: <SiYoutube />,
+	},
+	{
+		name: "Play",
+		url: "https://play.google.com",
+		icon: <SiGoogleplay />,
+	},
+	{
+		name: "Gmail",
+		url: "https://mail.google.com",
+		icon: <SiGmail />,
+	},
+	{
+		name: "Drive",
+		url: "https://drive.google.com",
+		icon: <SiGoogledrive />,
+	},
+	{
+		name: "Calendar",
+		url: "https://calendar.google.com",
+		icon: <SiGooglecalendar />,
+	},
+	{
+		name: "Gemini",
+		url: "https://gemini.google.com",
+		icon: <SiGooglegemini />,
+	},
+	{
+		name: "News",
+		url: "https://news.google.com",
+		icon: <SiGooglenews />,
+	},
+	{
+		name: "Photos",
+		url: "https://photos.google.com",
+		icon: <SiGooglephotos />,
+	},
+	{
+		name: "Meet",
+		url: "https://meet.google.com",
+		icon: <SiGooglemeet />,
+	},
+	{
+		name: "Translate",
+		url: "https://translate.google.com",
+		icon: <SiGoogletranslate />,
+	},
+	{
+		name: "Sheets",
+		url: "https://docs.google.com/spreadsheets",
+		icon: <SiGooglesheets />,
+	},
+	{
+		name: "Docs",
+		url: "https://docs.google.com/document",
+		icon: <SiGoogledocs />,
+	},
+	{
+		name: "Slides",
+		url: "https://docs.google.com/presentation",
+		icon: <SiGoogleslides />,
+	},
+	{
+		name: "Keep",
+		url: "https://keep.google.com",
+		icon: <SiGooglekeep />,
+	},
+	{
+		name: "Classroom",
+		url: "https://classroom.google.com",
+		icon: <SiGoogleclassroom />,
+	},
+	{
+		name: "Chat",
+		url: "https://chat.google.com",
+		icon: <SiGooglechat />,
+	},
+	{
+		name: "Earth",
+		url: "https://earth.google.com",
+		icon: <SiGoogleearth />,
+	},
+	{
+		name: "Saved",
+		url: "https://www.google.com/save",
+		icon: <Bookmark />,
+	},
+	{
+		name: "Google Ads",
+		url: "https://ads.google.com",
+		icon: <SiGoogleads />,
+	},
+	{
 		name: "Contacts",
-		url: "https://contacts.google.com/",
+		url: "https://contacts.google.com",
 		icon: <ContactsProduct />,
+	},
+	{
+		name: "Forms",
+		url: "https://docs.google.com/forms",
+		icon: <SiGoogleforms />,
+	},
+	{
+		name: "Chrome Web Store",
+		url: "https://chrome.google.com/webstore",
+		icon: <SiChromewebstore />,
+	},
+	{
+		name: "Password Manager",
+		url: "https://passwords.google.com",
+		icon: <VpnKey />,
+	},
+	{
+		name: "Google Analytics",
+		url: "https://analytics.google.com",
+		icon: <SiGoogleanalytics />,
+	},
+	{
+		name: "Blogger",
+		url: "https://www.blogger.com",
+		icon: <SiBlogger />,
+	},
+	{
+		name: "YouTube Music",
+		url: "https://music.youtube.com",
+		icon: <SiYoutubemusic />,
+	},
+	{
+		name: "Wallet",
+		url: "https://wallet.google.com",
+		icon: <Wallet />,
+	},
+	{
+		name: "NotebookLM",
+		url: "https://notebooklm.google.com",
+		icon: <SiNotebooklm />,
+	},
+	{
+		name: "Tasks",
+		url: "https://tasksboard.com",
+		icon: <Task />,
 	},
 ];
