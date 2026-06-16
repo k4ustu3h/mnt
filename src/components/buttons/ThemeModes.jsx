f6368736b4fae26bc955669cacc9fd27c5e15172
Kaustubh Ladiya <k4ustu3h@gmail.com>
2026-06-16 14:31:51 +0530

    Add icons to Theme Modes Segmented Button

--- src/components/buttons/ThemeModes.jsx
+++ src/components/buttons/ThemeModes.jsx
@@ -4,6 +4,11 @@ import {
 } from "@m3e/react/segmented-button";
 
 import useSettings from "@/hooks/useSettings";
+import {
+	Computer,
+	DarkMode,
+	LightMode,
+} from "@nine-thirty-five/material-symbols-react/rounded";
 
 export default function ThemeModes() {
 	const { themeScheme, setThemeScheme } = useSettings();
@@ -14,18 +19,21 @@ export default function ThemeModes() {
 				onClick={() => setThemeScheme("dark")}
 				checked={themeScheme === "dark"}
 			>
+				<DarkMode slot="icon" />
 				Dark
 			</M3eButtonSegment>
 			<M3eButtonSegment
 				onClick={() => setThemeScheme("light")}
 				checked={themeScheme === "light"}
 			>
+				<LightMode slot="icon" />
 				Light
 			</M3eButtonSegment>
 			<M3eButtonSegment
 				onClick={() => setThemeScheme("auto")}
 				checked={themeScheme === "auto"}
 			>
+				<Computer slot="icon" />
 				System
 			</M3eButtonSegment>
 		</M3eSegmentedButton>
