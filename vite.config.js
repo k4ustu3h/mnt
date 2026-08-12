c50599759191dfb4e23abf54fbb19c36939f88e6
Kaustubh Ladiya <k4ustu3h@gmail.com>
2026-08-12 15:04:13 +0530

    Enable React Compiler

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+
+import babel from "@rolldown/plugin-babel";
+
 import path from "path";
 import { fileURLToPath } from "url";
 
@@ -7,7 +11,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		babel({
+			presets: [reactCompilerPreset()],
+		}),
+	],
 	resolve: {
 		alias: {
 			"@": path.resolve(__dirname, "./src"),
